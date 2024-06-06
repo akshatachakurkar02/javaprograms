2c1f2745b67dd350a191df2c0cd7c5909215342e
akshatachakurkar02 <chakurkarakshatavedant02@gmail.com>
2024-06-06 12:24:44 +0530

    my first rlease

---
+++ src/Firstjavaproject/firstprogram.java
@@ -0,0 +1,10 @@
+package Firstjavaproject;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		  System.out.println("Hello world");                                   // TODO Auto-generated method stub
+
+	}
+
+}
